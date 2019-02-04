cce0a93dd627484f46642e486d930bda09d9b788
Filippo Ferrari <f.ferrari95@gmail.com>
2019-02-04 13:19:40 +0100

    1:19

--- README.md
+++ README.md
@@ -1,12 +1,49 @@
-# The concept
-Endangered animals (and how to find them) (picture)
+# The idea
+
+![alt text](https://github.com/drawwithcode/2018-group-work-group-07/blob/master/endangered%20animals.png)
+
+### Who are we?
+This is a project created by Teresa Cremonesi, Andrea Febres Medina, Filippo Ferrari and Cristina Pita da Veiga developed in the course of Creative Coding at Politecnico di Milano.
+
 ### What is this?
-Say what the project is about
+This project is an interactive where the user has to find in the Amazonic jungle five different animals endangered or threatened using a variety of tools: the voice, the keyboard, the mouse... Thought the time they spend playing with it they will discover different characteristics of the animals and interact with the computer. We create also "fake animals" in the background that are not really in the jungle, and the user should discover whish are they. To active two tools you have to select them (banana and light) the other two you can be use without selectioning them. 
+
+*Banana*: clicking ober the banana the user active the tool and when is aproximiting to the monkey the forest move, so they can select over it.
+*Light*: some animals are sensible to the light (in this case the sloth). Activing the light its created a cover to search the animal moving the mouse.
+*Voice*: saying the name of the animals hidden the user will find two of them. In this way will appear the armadillo and the butterfly.
+*Keyboard*: pushing the space to move the plants and clicking over it to see the hidden animal to find the frog.
+
+
 ### Why this idea?
-Say what the project is about and where your idea comes from.
+This idea start as a claim about what is happening with the animals because of human. In the last years has increased the number of species in danger or threatened because of human acts and it has been declared as a “global epidemic”. Scientists estimate that up to 200 species die every hour. The deforestation of the Amazon, hunting and other human actions are destroying diverse species. 
+
+With this game the user discover some of the species that are in danger. This is a way to make people awareness about this problem. We have to create conciousness about our "global village".
+
 
 ## Inspirations/References
 All the other projects that inspired you, from any perspective (interactive, visual, critical, theorietical, etc)
+
+- Visual
+
+The idea was to create illustrations but not very childish style. At the end the idea is to concern people about a very big problem, the game can be fun with colourfull illustrations, but there is a black screen in the back remember the problem. 
+
+- Interactive projects inspiration
+
+![alt text](https://github.com/drawwithcode/2018-group-work-group-07/blob/master/inspi2000.png)
+
+Hidden folks is an inspiration for the project because of the idea of creating a big screen to find the animals.
+https://hiddenfolks.com/
+
+Species in pieces is a website where animals in danger are creating with different pieces geometricaly, in this way to can discover more about endangered animals.
+http://www.species-in-pieces.com/
+
+This project develoop by propublica give us an idea of what we wanted to talk about: animals in danger of extintion. From this information we achive the idea of the project.
+https://projects.propublica.org/extinctions/
+
+- Theorethical
+
+We search in different platforms the animals endangered or threatened and we select the most interesting for our projects. This are some of the sources: Proceedings of the National Academy of Sciences, World Wild Life, NYT, BBC, IUCN Red List of Threatened Animals (https://www.iucnredlist.org/). Also we search for articles about the topic that could make our story more deep. For example: the economics of finding the endangered species (http://www.rff.org/blog/2010/economics-endangered-species).
+
 
 ## --IMAGE/GIF/VIDEO--
 A visual recap of your project, it must convey main features and make people want to look at the real sketch
@@ -17,10 +54,60 @@ A visual recap of your project, it must convey main features and make people wan
 Explain choiches behind the design of interactions and the architecture of the sketch. What are the most innovative elements?
 If useful, do the same for the visual design of the sketch.
 
+- Design the jungle and the animals
+
+One of the main channels was to put the big screen and the load of all the pictures. How to manage all the png and the levels of animals.
+
+![alt text](https://github.com/drawwithcode/2018-group-work-group-07/blob/master/sfondo.png) 
+
+
 ## Code challenges 
 challenges On the technical side, what drove you crazy. What are the most interesting and elegant solutions?
 
+- Big screen
+
+At the begginig we started with the idea of creating a big screen to move around. The main problem was if we create this part, we had to cut other parts, so we decided to create a screen with all the elements as Hidden Folks. 
+
+- Light
+
+The light was one of the main lights we wanted to create with the camera, but we found problems to create the reaction with the whole scrren (at the begginig the moving screen gave us a better interaction with light), so we decided to create a different way to use it. With a cover we develop a "light" to wake up the sloth.
+
+- Zoom and size screen
+
+We found the problem that when you zoom with the keyboard or reduze the size of the windows the background can change, so we create a "error" screen to avoid the change of the size.
+
+
 ## Credits (third libraries used)
 
-# Who are we?
-This is a project created by Teresa Cremonesi, Andrea Febres Medina, Filippo Ferrari and Cristina Pita da Veiga developed in the course of Creative Coding at Politecnico di Milano.
+
+
+https://processing.org/examples/
+
+
+
+### Information about the Amazon and the animals
+The Amazon is the world's biggest rainforest and is the home of the 10% of species in the planet. Nearly two-thirds of the Amazon rainforest is found in Brazil, but it also cover other countries as Colombia, Peru or Bolivia. The Amazon River is, by far, the world's largest river by volume: 7.000 kilometers.But this incredible rainforest has a very big problem: human action. Unless the deforestation in the Amazon rainforest has been declining since 2004, is the biggest front in the world and, as explains WWF, interventions are needed to prevent a large-scale ecological disaster, as WWF explains. Right now, a 20% of the Amazon biome has already been lost.
+
+*Blue morpho butterfly  (Morpho menelaus)*
+ 
+This butterfly is one of the most known, emblematic and favorite of all Amazon species. They have a characteristic vivid blue color and shiny wings that reflect the light, making them easy to spot amidst the jungle canopy. The blue morpho is one of the largest butterflies in the world, measuring around six inches long and with wing spans of up to 20 centimetres. But there is a problem: they only life 115 days.
+
+*Sloth of three fingers (Bradypus variegatus)*
+
+The famous animal that always is sleeping is from the Amazon. The sloth of three fingers has short, stubby tails which they use when defecating, and three elongated fingers at the end of each limb. They have also the ability to turn their head through 270° which allows them to save energy while they are scanning for predators. In this way they don’t have to move their whole body. But, (surprise!) they are good swimmers, they have the ability to swim through water three times faster than they can move on the ground.
+ 
+*Black spider monkey (Ateles paniscus)*
+ 
+This cute monkey feeds on a wide variety of fruits and young leaves, flowers, buds, bark, honey, and occasionally small insects. They are agile primates, capable of moving swiftly through the trees by swinging, climbing, running along branches, or even walking bipedally. Other curious thing is that they can even hang suspended by the tail, leaving both hands free for feeding (they know how to distribute the work). This animal is very social, they live in groups of up to 20 to 30 individuals, divide into smaller or temporary subgroups. The only persistent relationship is between the female and offspring.
+
+*Giant armadillo (Priodontes maximus)*
+ 
+This curious animal is the largest of one of Earth’s most fascinating groups of armoured animals. The giant armadillo measures up to one meter long, exclusive of tail, with the tail adding another 50 centimetres, be careful, if you trip over him you can hurt yourself. It possesses more teeth than any other mammal (up to 100). Also, they are solitary individuals who feed mainly on termites. They are also good diggers and live in burrows in forests or scrub, emerging at night to feed. Yes, that’s not only vampires staff. 
+ 
+*Lemur tree frogs (Agalychnis lemur)*
+
+Surely you have seen this animal before, the lemur tree is bright yellowish-green to brown. They can change color depending on whether they are active or resting. For example, when they are active at night, they become more camouflaged as their upper parts turn to brown, also their eyes turn dark gray too. This is going to surprise you: they rarely jump unless fleeing from danger, they move slowly and walk hand over hand. And, this is gorgeous, these amphibians are part of the ‘tree frogs’ families, which have a fossil record in the Palaeocene (65 to 53 million years ago!).
+
+
+![alt text](https://github.com/drawwithcode/2018-group-work-group-07/blob/master/animali2000.png)
+
--- sketch.js
+++ sketch.js
@@ -12,15 +12,79 @@ var myImg11;
 var myImg12;
 var myImg13;
 var myImg14;
+var myImg15;
+var myImg16;
+var myImg17;
+var myImg18;
+var myImg19;
+var myImg20;
+var myImg21;
+var myImg22;
+var myImg23;
+var myImg24;
+var myImg25;
+var myImg26;
+var myImg27;
+var myImg28;
+var myImg29;
+var myImg30;
+var myImg31;
+var myImg32;
+var myImg33;
+var myImg34;
+var myImg35;
+var myImg36;
+var myImg37;
 var mySong;
 var button;
 let angle = 0.0;
 let jitter = 0.0;
+var rana = false;
+var farfalla = false;
+var scimmia = false;
+var armadillo = false;
+
+var myImg39; // hoja morada
+var myImg38; // cover negra con hueco
+var light = false; //sirve que sea asì
+var leavessloth = true; //igual que light
+
+//var myImage40;
+//var myImage41;
+var myImage42;
+var myImage43;
+var myImage44;
+var myImage45;
+var myImage46;
+
+
+var leavemonkey = true;
+var leaves = true;
+var banana = false;
+
+var info = false;
+var xinfo = false;
+
+var SpeechRec;
+var analyzer;
+
+var instructions = false;
+var intro = false;
+var startgame = false;
+var sfondo = false;
+
+var myInstructions;
+var myIntro;
+var avanti1;
+var avanti2;
+
+var bradipo;
+var armadillo;
 
 function preload() {
   mySong = loadSound("./library/sound.mp3");
 
-  myImage = loadImage("./library/sfondo.png");
+  myImage = loadImage("./library/fondo.png");
   myImage2 = loadImage("./library/covermariposa.png");
   myImage3 = loadImage("./library/coverrana.png");
   myImage4 = loadImage("./library/coverarmadillo.png");
@@ -34,137 +98,480 @@ function preload() {
   myImage12 = loadImage("./library/luce.png");
   myImage13 = loadImage("./library/banner.png");
   myImage14 = loadImage("./library/liana.png");
+  myImage15 = loadImage("./library/farfalla.png");
+  myImage16 = loadImage("./library/rana.png");
+  myImage17 = loadImage("./library/armadillo.png");
+  myImage18 = loadImage("./library/bradipo.png");
+  myImage19 = loadImage("./library/scimmia.png");
+  myImage20 = loadImage("./library/sfondofinale.png");
+  myImage21 = loadImage("./library/ranafinale.png");
+  myImage22 = loadImage("./library/armadillofinale.png");
+  myImage23 = loadImage("./library/farfallafinale.png");
+  myImage24 = loadImage("./library/bradipofinale.png");
+  myImage25 = loadImage("./library/scimmiafinalepng.png");
+  myImage26 = loadImage("./library/a1.png");
+  myImage27 = loadImage("./library/L1.png");
+  myImage28 = loadImage("./library/c2.png");
+  myImage29 = loadImage("./library/g4.png");
+  myImage30 = loadImage("./library/u1.png");
+  myImage31 = loadImage("./library/s3.png");
+  myImage32 = loadImage("./library/i2.png");
+  myImage33 = loadImage("./library/testoarmadillo.png");
+  myImage34 = loadImage("./library/testobradipo.png");
+  myImage35 = loadImage("./library/testofarfalla.png");
+  myImage36 = loadImage("./library/testorana.png");
+  myImage37 = loadImage("./library/testoscimmia.png");
+  myImage38 = loadImage("./library/cover.png");
+  myImage39 = loadImage("./library/copribradipo.png");
+  //myImage40 = loadImage('./library/scimmia.png');
+  //myImage41 = loadImage('./library/coperturascimmia1.png');
+  myImage42 = loadImage('./library/s1.png');
+  myImage43 = loadImage('./library/z4.png');
+  myImage44 = loadImage('./library/z5.png');
+  myImage45 = loadImage('./library/z6.png');
+  myImage46 = loadImage('./library/banana.png');
+  myImage47 = loadImage('./library/info.png');
+
+  bottoneplay = loadImage("./library/boton.png");
+
+  myInstructions = loadImage("./library/istruzioni.png");
+  myIntro = loadImage("./library/intro.png");
+  avanti1 = loadImage("./library/avanti_1.png");
+  avanti2 = loadImage("./library/avanti_2.png");
 
 
+}
 
-  //button iconi
-  button = createImg("./library/exppng.png");
-  button.position(50, 50);
-  button.size(50,50)
-  button.mousePressed(menu)
+function setup() {
 
-  //button inicio
-  button = createImg("./library/boton.png");
-  button.position(650, 600);
-  button.size(100,30)
-  button.mousePressed(play)
+  createCanvas(windowWidth, windowHeight);
+  frameRate(25);
+
+  //speech recognition
+  var speechRec = new p5.SpeechRec('en-US', parseResult);
+
+  var continuous = true;
+  var interim = true;
+
+  speechRec.start(continuous, interim);
+
+  function gotSpeech() {
+    if (speechRec.resultValue) {
+      createP(speechRec.resultString);
+    }
+    console.log(speechRec);
+  }
+
+  function parseResult() {
+
+    var mostrecentword = speechRec.resultString.split(' ').pop();
+
+    if (mostrecentword.indexOf("sloth") !== -1) {
+      bradipo.shake();
+    } else if (mostrecentword.indexOf("armadillo") !== -1) {
+      armadillo.shake();
+    } else if (mostrecentword.indexOf("green") !== -1) {
+      fill(0, 255, 0);
+      ellipse(width / 2, height / 4, 200);
+    } else if (mostrecentword.indexOf("blue") !== -1) {
+      fill(0, 0, 255);
+      ellipse(width / 2, height / 4, 200);
+    }
+
+  }
+
+  // analyzer
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(mySong);
+
+  bradipo = new Bradipo();
+  armadillo = new Armadillo();
 
 
 
 }
 
-function setup () {
 
-  createCanvas(windowWidth,windowHeight)
-  //background(myImage)
-  mySong.play();
+function draw() {
+
+  //schermate iniziali
+  if (sfondo) {
+    if (instructions) {
+      if (startgame) {
+        image(myImage, 0, 0, windowWidth, windowHeight); //sfondo gioco
+        bradipo.display();
+        armadillo.display();
+      } else {
+        image(myInstructions, 0, 0, windowWidth, windowHeight); //sfondo instructions
+        push();
+        imageMode(CENTER);
+        image(avanti2, windowWidth / 7 * 5.85, windowHeight / 10 * 7.6, 30, 30); //avanti2
+        pop();
+        // ellipse(windowWidth / 7 * 5.85, windowHeight / 10 * 7.6, 30);
+      }
+    } else {
+      image(myIntro, 0, 0, windowWidth, windowHeight); //sfondo intro
+      // ellipse(windowWidth / 7 * 5.65, windowHeight / 10 * 7.6, 30);
+      push();
+      imageMode(CENTER);
+      image(avanti1, windowWidth / 7 * 5.65, windowHeight / 10 * 7.6, 30, 30); //avanti1
+      pop();
+    }
+  } else {
+    image(myImage7, 0, 0, windowWidth, windowHeight); //sfondo inizio
+    push();
+    imageMode(CENTER);
+    image(bottoneplay, windowWidth / 2, windowHeight / 10 * 7.6, 100, 30);
+    pop();
+  }
+
+  //music when you start the game
+  if (mouseClicked == true) {
+    mySong.stop();
+  } else {
+    if (mySong.isPlaying() == false) {
+      mySong.play()
+    }
+  }
+
+  //map volume
+  volume = analyzer.getLevel();
+  volume = map(volume, 0, 1, 0, height);
+
+  //farfalla
+  image(myImage15, 0, 0, windowWidth, windowHeight);
+
+  //cover farfalla
+  //image(myImage2, 0,0,windowWidth,windowHeight);
+
+  //rana
+  image(myImage16, 0, 0, windowWidth, windowHeight);
+
+  //cover rana
+  //image(myImage3,0,0,windowWidth,windowHeight);
+
+  //armadillo
+  image(myImage17, 0, 0, windowWidth, windowHeight);
+
+  //cover armadillo
+  //image(myImage4, 0,0,windowWidth,windowHeight);
+
+  // bradipo
+  image(myImage18, 0, 0, windowWidth, windowHeight);
+
+  //cover bradipo
+  //image(myImage5, 0,0,windowWidth,windowHeight);
+
+  // scimmia
+  image(myImage19, 0, 0, windowWidth, windowHeight);
+
+  //cover scimmia
+  //image(myImage6, 0,0,windowWidth,windowHeight);
+
+  //icono banner
+  image(myImage13, windowWidth / 4.8, windowHeight / 13, windowWidth / 1.8, windowHeight / 13);
+
+  //icono suono
+  image(myImage9, 0, 0, windowWidth, windowHeight);
+
+  //icono binoculo
+  image(myImage10, 0, 0, windowWidth, windowHeight);
+
+  //icono tastiera
+  image(myImage11, 0, 0, windowWidth, windowHeight);
+
+  //luce
+  image(myImage12, 0, 0, windowWidth, windowHeight);
 
 
-}
+  //primera pagina
+  //image(myImage7, 0, 0, windowWidth, windowHeight);
 
 
-
-
-
-function draw () {
-  background(myImage)
-
-//image(myImage, 0, 0, myImage.width/3, myImage.height/3);
-
-
-//cover mariposa
-
-image(myImage2, 208, 125, 75,75);
-
-//cover rana
-
-image(myImage3, 200, 660, 50, 50);
-
-//cover armadillo
-
-image(myImage4, 735, 600, 160, 160);
-
-//cover bradipo
-
-image(myImage5, 767, -35, 125, 430);
-
-//cover simio
-
-image(myImage6, 855, 380, 190, 190);
-
-//icono banner
-image(myImage13, 370, 50, 700, 50);
-
-//icono suono
-image(myImage9, 330, -105, 700, 400);
-
-//icono binoculo
-image(myImage10, 360,-105, 700, 400);
-
-//icono tastiera
-image(myImage11, 380, -105, 700, 400);
-
-//luce
-image(myImage12, 400, -105, 700, 400);
-
-
-
-
-//primera pagina
-//image(myImage7, 0, 0, myImage.width/2.7, myImage.height/2.7);
-
-
-
-//texto inicio
-	// push()
-	// textFont('Rubik:300')
-	// fill(250)
-	// textAlign(CENTER)
-	// textSize(15)
-	// text('In the last years has increased the number of species in danger or threatened because of human acts and it has been declared as a “global epidemic”. Scientists estimate that up to 200 species die every hour. The deforestation of the Amazon, hunting and other human actions are destroying diverse species. With this game you will discover some of the species that are in danger. Choose different tools and discover more about animals in danger in the Amazon.',width/2,height/2.5)
-	// pop()
-
+  //herramienta keyboard
   push()
-  //movimento liana amarilla
-  if (second() % 1 === 0) {
-    jitter = random(0, 0.1);
-   }
-   angle = angle + jitter;
-    let c = cos(angle);
-
-  translate(0,0)
-   rotate(c);
-   image(myImage14,-100,-100,1000,500);
+  if (keyIsPressed == true) { // If the key is pressed,
+    image(myImage3, 0, 0, windowWidth, windowHeight);
+    image(myImage26, 10, 0, windowWidth, windowHeight);
+    image(myImage27, 400, -30, windowWidth, windowHeight);
+    image(myImage28, 700, -130, windowWidth, windowHeight)
+    image(myImage29, 700, 0, windowWidth, windowHeight)
+    image(myImage29, 0, 0, windowWidth, windowHeight)
+    image(myImage30, 0, -10, windowWidth, windowHeight)
+    image(myImage31, 0, 0, windowWidth, windowHeight)
+    image(myImage32, 0, 0, windowWidth, windowHeight)
+  } else {
+    image(myImage3, 2, 0, windowWidth, windowHeight);
+    image(myImage26, 8, 2, windowWidth, windowHeight);
+    image(myImage27, 400, 0, windowWidth, windowHeight);
+    image(myImage28, 715, -135, windowWidth, windowHeight)
+    image(myImage29, 700, 5, windowWidth, windowHeight)
+    image(myImage29, 3, 0, windowWidth, windowHeight)
+    image(myImage30, 0, 5, windowWidth, windowHeight)
+    image(myImage31, 0, 5, windowWidth, windowHeight)
+    image(myImage32, 5, 2, windowWidth, windowHeight)
+  }
   pop()
 
+
+  //aparición rana
+  if (rana) {
+    image(myImage16, 0, 0, windowWidth, windowHeight)
+
+  } else {
+    image(myImage3, 0, 0, windowWidth, windowHeight)
+
+  }
+
+  //aparicion farfalla
+  if (farfalla) {
+    image(myImage15, 0, 0, windowWidth, windowHeight)
+
+  } else {
+    image(myImage2, 0, 0, windowWidth, windowHeight)
+  }
+
+  //aparición scimmia
+  if (scimmia) {
+    image(myImage19, 0, 0, windowWidth, windowHeight)
+
+  } else {
+    image(myImage6, 0, 0, windowWidth, windowHeight)
+
+  }
+
+  //aparición armadillo
+  if (armadillo) {
+    image(myImage17, 0, 0, windowWidth, windowHeight)
+
+  } else {
+    image(myImage4, 0, 0, windowWidth, windowHeight)
+
+  }
+
+  // banna + scimmmia
+  image(myImage19, 0, 0, windowWidth, windowHeight);
+
+  if (leavemonkey) {
+    image(myImage6, 0, 0, windowWidth, windowHeight);
+  }
+
+  if (leaves) {
+
+    image(myImage43, 0, 0, windowWidth, windowHeight);
+    image(myImage44, 0, 0, windowWidth, windowHeight);
+    image(myImage45, 0, 0, windowWidth, windowHeight);
+  }
+
+  if (mouseX >= windowWidth / 1.5 - windowWidth / 8 && mouseX <= windowWidth / 1.5 + windowWidth / 8 && mouseY >= windowHeight / 1.6 - windowHeight / 8 && mouseY <= windowHeight / 1.6 + windowHeight / 8) {
+    var x = random(2);
+    var y = random(2);
+    image(myImage43, x, y, windowWidth, windowHeight);
+    image(myImage44, x, y, windowWidth, windowHeight);
+    image(myImage45, x, y, windowWidth, windowHeight);
+  }
+
+  if (banana) {
+    image(myImage46, mouseX, mouseY, windowWidth / 15, windowHeight / 10);
+  }
+
+  ellipse(windowWidth / 1.77, windowHeight / 9, 20);
+
+  //aparicion sloth
+
+  if (leavessloth) {
+    var x = random(1);
+    var y = random(1);
+    image(myImage5, x, y, windowWidth, windowHeight);
+    image(myImage39, x, y, windowWidth, windowHeight);
+  }
+
+  // da cancellare poi: è solo per fare il tool della luce
+  fill(250)
+  ellipse(windowWidth / 3, windowHeight / 9, 20);
+
+
+  if (light) {
+    image(myImage38, mouseX - myImage38.width / 2, mouseY - myImage38.height / 2, myImage38.width, myImage38.height);
+  }
+
+  //boton info
+  image(myImage8, 0, 0, windowWidth, windowHeight)
+
+  if (info) {
+    image(myImage47, 0, 0, windowWidth, windowHeight)
+
+  }
+  fill(250)
+  ellipse(windowWidth / 1.065, windowHeight / 10, 30);
+
+  //boton x
+  if (xinfo) {
+    image(myImage47, 0, 0, windowWidth, windowHeight)
+
+  }
+  fill(250)
+  ellipse(windowWidth / 1.15, windowHeight / 9, 30);
+
+  // error if the window is too small
+  if (windowWidth < 1100 || windowHeight < 620) {
+    push();
+    rectMode(CENTER);
+    rect(windowWidth / 2, windowHeight / 2, windowWidth, windowHeight);
+    textAlign(CENTER);
+    textSize(100);
+    text('Error', windowWidth / 2, 300);
+    fill(255, 0, 0);
+    pop();
+  }
+
+  //modify mouse cursor
+  var dforward1 = dist(mouseX, mouseY, windowWidth / 7 * 5.85, windowHeight / 10 * 7.6);
+  var dforward2 = dist(mouseX, mouseY, windowWidth / 7 * 5.65, windowHeight / 10 * 7.6);
+  var dforward3 = dist(mouseX, mouseY, windowWidth / 7 * 5.45, windowHeight / 10 * 7.6);
+  if (dforward1 < 15 || dforward2 < 15 || dforward3 < 15 || mouseX > windowWidth / 2 - 50 && mouseX < windowWidth / 2 + 50 && mouseY > windowHeight / 10 * 7.6 - 15 && mouseY < windowHeight / 10 * 7.6 + 15) {
+    cursor(HAND);
+  } else {
+    cursor(ARROW);
+  }
+
+  ////pagina final info animali
+  // image(myImage20, 0,0,windowWidth,windowHeight);
+  // image(myImage21, 0,0,windowWidth,windowHeight);
+  // image(myImage22, 0,0,windowWidth,windowHeight);
+  // image(myImage23, 0,0,windowWidth,windowHeight);
+  // image(myImage24, 0,0,windowWidth,windowHeight);
+  // image(myImage25, 0,0,windowWidth,windowHeight);
+  //
+  // //info rana
+  // if ((mouseX > windowWidth/10) && (mouseX < windowWidth/5)){
+  //   image(myImage36, 0,0,windowWidth,windowHeight);
+  // }
+  // //info armadillo
+  // if ((mouseX > windowWidth/5) && (mouseX < windowWidth/3)){
+  //   image(myImage33, 0,0,windowWidth,windowHeight);
+  // }
+  // //info bradipo
+  // if ((mouseX > windowWidth/2.8) && (mouseX < windowWidth/1.5)){
+  //   image(myImage34, 0,0,windowWidth,windowHeight);
+  // }
+  // //info farfalla
+  // if ((mouseX > windowWidth/1.5) && (mouseX < windowWidth/1.3)){
+  //   image(myImage35, 0,0,windowWidth,windowHeight);
+  // }
+  // //info scimmia
+  // if ((mouseX > windowWidth/1.3) && (mouseX < windowWidth/1.01)){
+  //   image(myImage37, 0,0,windowWidth,windowHeight);
+  // }
+
+
 }
-//boton menu
 
-function menu() {
+function Bradipo() {
 
-if(button.mousePressed(menu)){
-background(0)
-} else {
-background(myImage)
+  this.x = 767;
+  this.y = -35;
+
+  this.display = function() {
+    image(myImage5, this.x, this.y, 125, 430);
+  }
+
+  this.shake = function() {
+    this.x = this.x + random (-5, 5);
+  }
 }
 
+function Armadillo() {
 
+  this.x = 630;
+  this.y = 500;
+
+  this.display = function() {
+    image(myImage4, this.x, this.y, 160, 160);
+  }
+
+  this.shake = function() {
+    this.x = this.x + random (-5, 5);
+  }
 }
 
-//boton play
-function play() {
+//click rana + farfalla + scimmia + armadillo + bradipo
+function mouseClicked() {
 
-if(button.mousePressed(play)){
+  mySong.pause();
 
-background(0)
-} else {
-background(myImage);
+  //button play
+  if (mouseX > windowWidth / 2 - 50 && mouseX < windowWidth / 2 + 50 && mouseY > windowHeight / 10 * 7.6 - 15 && mouseY < windowHeight / 10 * 7.6 + 15) {
+    sfondo = true;
+  }
 
-}
+  //button avanti1
+  var dforward1 = dist(mouseX, mouseY, windowWidth / 7 * 5.85, windowHeight / 10 * 7.6);
+  if (dforward1 < 15 || dforward2 < 15 || dforward2 < 15) {
+    startgame = true;
+  }
 
+  //button avanti2
+  var dforward2 = dist(mouseX, mouseY, windowWidth / 7 * 5.65, windowHeight / 10 * 7.6);
+  if (dforward2 < 15) {
+    instructions = true;
+  }
+
+  var dirana = dist(mouseX, mouseY, windowWidth / 6.5, windowHeight / 12 * 11.3)
+  if (dirana < 40) {
+    rana = true
+
+  }
+  var difarfalla = dist(mouseX, mouseY, windowWidth / 6.2, windowHeight / 5)
+  if (difarfalla < 40) {
+    farfalla = true
+  }
+  var discimmia = dist(mouseX, mouseY, windowWidth / 1.5, windowHeight / 1.6)
+  if (discimmia < 50) {
+    scimmia = true
+  }
+  var diarmadillo = dist(mouseX, mouseY, windowWidth / 1.8, windowHeight / 12 * 11.3)
+  if (diarmadillo < 40) {
+    armadillo = true
+  }
+
+  var dlight = dist(mouseX, mouseY, windowWidth / 3, windowHeight / 9);
+  if (dlight < 20) {
+    light = true;
+  }
+
+  var dsloth = dist(mouseX, mouseY, windowWidth / 1.7, windowHeight / 3);
+  if (dsloth < 50) {
+    leavessloth = false;
+    light = false;
+  }
+
+  //bannan + scimmia
+  var dmonkey = dist(mouseX, mouseY, windowWidth / 1.5, windowHeight / 1.6);
+  if (dmonkey < windowWidth / 20) {
+    leavemonkey = false;
+    banana = false;
+  }
+
+  var dbanana = dist(mouseX, mouseY, windowWidth / 1.77, windowHeight / 9);
+  if (dbanana < 20) {
+    banana = true;
+  }
+  //boton info
+  var diinfo = dist(mouseX, mouseY, windowWidth / 1.065, windowHeight / 10)
+  if (diinfo < 50) {
+    info = true
+
+  }
+
+  //cerrar boton info
+  var dxinfo = dist(mouseX, mouseY, windowWidth / 1.15, windowHeight / 9)
+  if (dxinfo < 50) {
+    info = false
+  }
 }
 
 
 function windowResized() {
-  resizeCanvas(windowWidth,windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
